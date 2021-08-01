5266dd1e87589cf4ff7dd0f72b76f5e2b1d12c8b
Wouter Luberti <wouter.luberti@xs4all.net>
2021-08-01 22:05:33 +0200

    web router order logic improvement for Breeze routes

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
+require __DIR__.'/auth.php';
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -24,5 +22,3 @@ Route::get('/dashboard', function () {
 Route::get('/{any}', function () {
     return view('app');
 })->where('any', '.*');
-
-require __DIR__.'/auth.php';
