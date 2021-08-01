47534b6f7788776a6b04df1342f3f919f6e81cb4
Wouter Luberti <wouter.luberti@xs4all.net>
2021-08-01 21:55:43 +0200

    make new settings deployable

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+Route::get('/{any}', function () {
+    return view('app');
+})->where('any', '.*');
 
 require __DIR__.'/auth.php';
